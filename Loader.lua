4a0e373422c48de0c59e372fa70e0b531e71bab9
MrAntiMatteryAlt <72738859+MrAntiMatteryAlt@users.noreply.github.com>
2021-03-04 15:13:43 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -93,7 +93,7 @@ local LOADSTRING_Function = function()
 		};
 		[301549746] = {
 			['name'] = 'CB:RO';
-			['func'] = function() loadstring(game:HttpGet('https://pastebin.com/raw/b9XQGh8x',true))() end;
+			['func'] = function() loadstring(game:HttpGet('https://raw.githubusercontent.com/MrAntiMatteryAlt/No/Games/CBRO.lua',true))() end;
 		};
 		[5958644308] = {
 			['name'] = 'RO:BA';
