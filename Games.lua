f6f0ed443ee3d260b9a1935fb11baed43c9d0cb8
MrAntiMatteryAlt <72738859+MrAntiMatteryAlt@users.noreply.github.com>
2021-04-23 22:10:42 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -11,6 +11,26 @@ local games = {
 			['name'] = 'RO:BA';
 			['func'] = function() loadstring(game:HttpGet('https://pastebin.com/raw/pKU4FCR4',true))() end;
 		};
+		[1537690962] = {
+			['name'] = 'Bee Swarm Simulator';
+			['func'] = function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrAntiMatteryAlt/No/Games/Bee%20Swarm%20Simulator.lua'),true))() end
+		};
+		[2482834103] = {
+			['name'] = 'Blob Simulator';
+			['func'] = function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrAntiMatteryAlt/No/Games/Blob%20Simulator.lua'),true))() end;
+		};
+		[3956818381] = {
+			['name'] = 'Ninja Legends';
+			['func'] = function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrAntiMatteryAlt/No/Games/Ninja%20Legends.lua'),true))() end; 
+		};
+		[5977280685] = {
+			['name'] = 'Ninja Legends 2';
+			['func'] = function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrAntiMatteryAlt/No/Games/Ninja%20Legends%202.lua'),true))() end;
+		};
+		[155615604] = {
+			['name'] = 'Prison Life';
+			['func'] = function() loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrAntiMatteryAlt/No/Games/Prison%20Life.lua'),true))() end;
+		};
 }
 
 return games
